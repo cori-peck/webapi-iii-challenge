18b0f82a21754b9b77c297d13450957ad11d44b9
cori-peck <cori.peck@ymail.com>
2019-03-20 16:14:49 -0500

    U-Update is working, but in the put postman tab returnng an error but get is returning the updated user

--- index.js
+++ index.js
@@ -18,3 +18,45 @@ server.get('/api/users', (req, res) => {
         res.status(500).json({ error: 'Cannot reach user list'})
     })
 })
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const user = req.body;
+    if (!user.name) {
+        res.status(400).json({ error: 'Username needed' })
+    } else {
+        userDb
+        .get(id)
+        .then(users => {
+            console.log(users)
+            if (users.length < 1) {
+                return res.status(404).json({ message: 'The user with the specified ID does not exist'})
+            } else {
+                userDb
+                .update(id, user)
+                .then(res => res.status(200).json({ user }))
+                .catch(err => {
+                    res.status(400).json({ error: "Did not update"})
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "Id does not exist" })
+        })
+    }
+})
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name) {
+        res.status(400).json({ error: 'Username needed' })
+    } else {
+        userDb
+        .insert(user)
+        .then(res => res.status(201).json(user))
+        .catch(err => {
+            res.status(500).json({ error: 'User could not be added' })
+        })
+    }
+})
