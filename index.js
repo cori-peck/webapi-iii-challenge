396238708d8ff9ea3eab666f86935d268d60829d
cori-peck <cori.peck@ymail.com>
2019-03-20 15:29:26 -0500

    R-Read: user list is obtained with .get

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // code away!
 const express = require('express');
 const server = express();
+const userDb = require('./data/helpers/userDb');
 
 server.use(express.json())
 
 
 server.listen(8000, () => console.log('Server up and running on port 8k'))
+
+
+server.get('/api/users', (req, res) => {
+    userDb
+    .get()
+    .then(users => res.status(200).json({ users }))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: 'Cannot reach user list'})
+    })
+})
