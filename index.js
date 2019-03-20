06cb01bed408e69c2eff089666546b93ff69fef7
cori-peck <cori.peck@ymail.com>
2019-03-20 15:18:27 -0500

    Initialized project - server running on port 8000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+const express = require('express');
+const server = express();
+
+server.use(express.json())
+
+
+server.listen(8000, () => console.log('Server up and running on port 8k'))
